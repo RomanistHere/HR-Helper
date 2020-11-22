45c123543d0ed3da77fee04589dfff91772dc840
RomanistHere <romanisthere@gmail.com>
2020-11-22 19:23:03 +0300

    update linkedin

--- content.js
+++ content.js
@@ -1,3 +1,18 @@
+const debounce = (func, wait, immediate) => {
+	var timeout
+	return function() {
+		var context = this, args = arguments
+		var later = function() {
+			timeout = null
+			if (!immediate) func.apply(context, args)
+		}
+		var callNow = immediate && !timeout
+		clearTimeout(timeout)
+		timeout = setTimeout(later, wait)
+		if (callNow) func.apply(context, args)
+	}
+}
+
 const fillText = (item, text) => {
     const textArea = item.querySelector('.RomanistHere__textarea')
     textArea.value = text
@@ -27,19 +42,38 @@ const appendElements = (item, name) => {
             chrome.storage.sync.set({ data: newData })
         })
     })
+
+    item.classList.add('RomanistHere-filled')
 }
 
-chrome.storage.sync.get(['data'], resp => {
-    const { data } = resp
-    const items = document.querySelectorAll('.search-result.search-result--person')
+const updInfo = () => {
+    console.warn('updinfo')
+    chrome.storage.sync.get(['data'], resp => {
+        const { data } = resp
+        const items = document.querySelectorAll('.search-result.search-result--person:not(.RomanistHere-filled)')
 
-    items.forEach(item => {
-        const name = item.querySelector('.name.actor-name').textContent
+        items.forEach(item => {
+            const name = item.querySelector('.name.actor-name').textContent
 
-        appendElements(item, name)
+            appendElements(item, name)
 
-        if (name in data) {
-            fillText(item, data[name])
-        }
+            if (name in data) {
+                fillText(item, data[name])
+            }
+        })
     })
+}
+
+updInfo()
+
+const debUpdInfo = debounce(updInfo, 500)
+
+const domObserver = new MutationObserver(mutations => {
+    console.log('mutation')
+    debUpdInfo()
+})
+
+domObserver.observe(document.documentElement, {
+    childList: true,
+    subtree: true
 })
